a32610f10acacae54693713684e2d178c88e364f
utkuonursahin <sahinutkuonur@gmail.com>
2025-06-02 11:32:04 +0300

    Add pagination controls to history table with first and last page buttons

--- src/app/user/history/table/page.tsx
+++ src/app/user/history/table/page.tsx
@@ -184,6 +184,13 @@ export default function HistoryTablePage() {
                         <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
                             <div className="flex-1 flex justify-between sm:hidden">
                                 <button
+                                    onClick={() => handlePageChange(1)}
+                                    disabled={currentPage === 0}
+                                    className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50"
+                                >
+                                    İlk
+                                </button>
+                                <button
                                     onClick={() => handlePageChange(currentPage)}
                                     disabled={currentPage === 0}
                                     className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50"
@@ -196,6 +203,13 @@ export default function HistoryTablePage() {
                                     className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50"
                                 >
                                     Sonraki
+                                </button>
+                                <button
+                                    onClick={() => handlePageChange(totalPages)}
+                                    disabled={currentPage === totalPages - 1}
+                                    className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50"
+                                >
+                                    Son
                                 </button>
                             </div>
                             <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
@@ -211,35 +225,80 @@ export default function HistoryTablePage() {
                                 <div>
                                     <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
                                         <button
-                                            onClick={() => handlePageChange(currentPage)}
+                                            onClick={() => handlePageChange(1)}
                                             disabled={currentPage === 0}
                                             className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50"
+                                        >
+                                            <span className="sr-only">İlk</span>
+                                            <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fillRule="evenodd" d="M13 15V5a1 1 0 10-2 0v10a1 1 0 102 0zM7 15V5a1 1 0 10-2 0v10a1 1 0 102 0z" clipRule="evenodd" />
+                                            </svg>
+                                        </button>
+                                        <button
+                                            onClick={() => handlePageChange(currentPage)}
+                                            disabled={currentPage === 0}
+                                            className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50"
                                         >
                                             <span className="sr-only">Önceki</span>
                                             <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                 <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
                                             </svg>
                                         </button>
-                                        {[...Array(totalPages)].map((_, index) => (
-                                            <button
-                                                key={index}
-                                                onClick={() => handlePageChange(index + 1)}
-                                                className={`relative inline-flex items-center px-4 py-2 border text-sm font-medium ${currentPage === index
-                                                    ? 'z-10 bg-blue-50 border-blue-500 text-blue-600'
-                                                    : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50'
-                                                    }`}
-                                            >
-                                                {index + 1}
-                                            </button>
-                                        ))}
+                                        {/* Sayfa numaraları */}
+                                        {(() => {
+                                            const pageButtons = [];
+                                            const maxPageButtons = 3; // Mevcut sayfa ve etrafındaki 1'er sayfa
+                                            let startPage = Math.max(1, currentPage + 1 - 1);
+                                            let endPage = Math.min(totalPages, currentPage + 1 + 1);
+                                            if (currentPage + 1 === 1) {
+                                                endPage = Math.min(totalPages, startPage + maxPageButtons - 1);
+                                            } else if (currentPage + 1 === totalPages) {
+                                                startPage = Math.max(1, endPage - maxPageButtons + 1);
+                                            }
+                                            if (startPage > 1) {
+                                                pageButtons.push(
+                                                    <span key="start-ellipsis" className="px-2 py-2 text-gray-400">...</span>
+                                                );
+                                            }
+                                            for (let i = startPage; i <= endPage; i++) {
+                                                pageButtons.push(
+                                                    <button
+                                                        key={i}
+                                                        onClick={() => handlePageChange(i)}
+                                                        className={`relative inline-flex items-center px-4 py-2 border text-sm font-medium ${currentPage === i - 1
+                                                            ? 'z-10 bg-blue-50 border-blue-500 text-blue-600'
+                                                            : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50'
+                                                            }`}
+                                                    >
+                                                        {i}
+                                                    </button>
+                                                );
+                                            }
+                                            if (endPage < totalPages) {
+                                                pageButtons.push(
+                                                    <span key="end-ellipsis" className="px-2 py-2 text-gray-400">...</span>
+                                                );
+                                            }
+                                            return pageButtons;
+                                        })()}
                                         <button
                                             onClick={() => handlePageChange(currentPage + 2)}
                                             disabled={currentPage === totalPages - 1}
-                                            className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50"
+                                            className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50"
                                         >
                                             <span className="sr-only">Sonraki</span>
                                             <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                 <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                                            </svg>
+                                        </button>
+                                        <button
+                                            onClick={() => handlePageChange(totalPages)}
+                                            disabled={currentPage === totalPages - 1}
+                                            className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50"
+                                        >
+                                            <span className="sr-only">Son</span>
+                                            <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fillRule="evenodd" d="M7 5v10a1 1 0 102 0V5a1 1 0 10-2 0zm6 0v10a1 1 0 102 0V5a1 1 0 10-2 0z" clipRule="evenodd" />
                                             </svg>
                                         </button>
                                     </nav>
